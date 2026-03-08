45adfc9549b2facbf487132ddaf766ad672a22bd
sarveshmalik3110 <sarveshmalik3110@gmail.com>
2026-03-08 19:32:36 +0530

    average delivery time by state for e-commerce analytics

---
+++ 10_graphs/016_Average_Delivery_Time_by_State.sql
@@ -0,0 +1,16 @@
+WITH delivery_time AS (
+SELECT
+c.customer_state,
+DATEDIFF(o.order_delivered_customer_date,
+o.order_purchase_timestamp) AS delivery_days
+FROM orders o
+JOIN customers c
+ON o.customer_id = c.customer_id
+WHERE o.order_delivered_customer_date IS NOT NULL
+)
+
+SELECT
+customer_state,
+AVG(delivery_days) AS avg_delivery_days
+FROM delivery_time
+GROUP BY customer_state;
