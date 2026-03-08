c9d7f726c882dbb3a063bbdadf37461aa56e3d88
sarveshmalik3110 <sarveshmalik3110@gmail.com>
2026-03-08 19:12:14 +0530

    moving average revenue for e-commerce analytics

---
+++ 09_time_series_analysis/002_moving_average_revenue.sql
@@ -0,0 +1,19 @@
+WITH daily_revenue AS (
+    SELECT
+        DATE(o.order_purchase_timestamp) AS order_date,
+        SUM(oi.price) AS revenue
+    FROM orders o
+    JOIN order_items oi
+    ON o.order_id = oi.order_id
+    GROUP BY order_date
+)
+
+SELECT
+    order_date,
+    revenue,
+    AVG(revenue) OVER(
+        ORDER BY order_date
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS moving_avg_7_day
+FROM daily_revenue
+ORDER BY order_date;
