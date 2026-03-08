843ba5e2dc02f45ca974a59e6fb6d23e950f428c
sarveshmalik3110 <sarveshmalik3110@gmail.com>
2026-03-08 19:22:43 +0530

    daily revenue trend for e-commerce analytics

---
+++ 10_graphs/002_Daily_Revenue_Trend.sql
@@ -0,0 +1,13 @@
+WITH daily_revenue AS (
+SELECT
+DATE(o.order_purchase_timestamp) AS order_date,
+SUM(oi.price) AS revenue
+FROM orders o
+JOIN order_items oi
+ON o.order_id = oi.order_id
+GROUP BY order_date
+)
+
+SELECT *
+FROM daily_revenue
+ORDER BY order_date;
