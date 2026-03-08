5de6bf80754d70a4c7f7dcd306ceec318d3d02c6
sarveshmalik3110 <sarveshmalik3110@gmail.com>
2026-03-08 19:31:50 +0530

    seller revenue ranking for e-commerce analytics

---
+++ 10_graphs/015_Seller_Revenue_Ranking.sql
@@ -0,0 +1,13 @@
+WITH seller_revenue AS (
+SELECT
+seller_id,
+SUM(price) AS revenue
+FROM order_items
+GROUP BY seller_id
+)
+
+SELECT
+seller_id,
+revenue,
+RANK() OVER(ORDER BY revenue DESC) AS seller_rank
+FROM seller_revenue;
